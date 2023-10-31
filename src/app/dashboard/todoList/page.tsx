b1494157af16fbdca0f8d16233d696c0cae8209a
na-yujin <kfoyujin@gmail.com>
2023-10-31 16:11:07 +0900

    feat: todoList 삭제버튼 작업완료

--- src/app/dashboard/todoList/page.tsx
+++ src/app/dashboard/todoList/page.tsx
@@ -2,10 +2,12 @@
 import styled from 'styled-components';
 import React, { useEffect, useState } from 'react';
 
+type TodoList = { id: number; text: string };
 export default function TodoListPage() {
   const [loading, setLoading] = useState<boolean>(true);
   const [todo, setTodo] = useState<string>('');
-  const [todos, setTodos] = useState<string[]>([]);
+  const [todos, setTodos] = useState<TodoList[]>([]);
+  const [listId, setListId] = useState<number>(0);
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTodo(event.target.value);
   };
@@ -14,9 +16,13 @@ export default function TodoListPage() {
     if (todo === '') {
       return;
     }
-    setTodos((currentArray) => [todo, ...currentArray]);
-    console.log(setTodos);
+    // setTodos((currentArray) => [todo, ...currentArray]);
+    setTodos([{ id: listId, text: todo }, ...todos]);
+    setListId((listId) => listId + 1);
     reset();
+  };
+  const onRemove = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
   };
   useEffect(() => {
     setLoading(false);
@@ -45,13 +51,16 @@ export default function TodoListPage() {
             </InputWrapper>
             <div>
               <ContentItemWrapper style={{ listStyle: 'none' }}>
-                {todos.map((item, index) => (
-                  <ContentItem style={{ display: 'flex' }}>
-                    <div key={index}>{item}</div>
-                    <div>
-                      <button>수정</button>
-                      <button>삭제</button>
-                    </div>
+                {todos.map((item) => (
+                  <ContentItem key={item.id}>
+                    <div>{item.text}</div>
+                    <ButtonWrapper>
+                      <ButtonSub>수정</ButtonSub>
+                      <ButtonSub onClick={() => onRemove(item.id)}>
+                        삭제
+                      </ButtonSub>
+                      {/*{item.id}*/}
+                    </ButtonWrapper>
                   </ContentItem>
                 ))}
               </ContentItemWrapper>
@@ -111,8 +120,9 @@ const ContentItemWrapper = styled.div`
   gap: 10px;
 `;
 const ContentItem = styled.div`
-  //border: 1px solid lightseagreen;
-  padding: 10px 0 10px 30px;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 30px 10px 30px;
   border-radius: 8px;
   background: #f3e8c7;
   color: #222;
@@ -122,4 +132,17 @@ const ContentItem = styled.div`
     position: absolute;
     transform: translateX(-20px);
   }
+`;
+const ButtonWrapper = styled.div`
+  display: flex;
+  gap: 4px;
+`;
+const ButtonSub = styled.button`
+  padding: 5px;
+  border-radius: 8px;
+  background: #ffb01b;
+  color: #fff;
+  border: none;
+  font-weight: 700;
+  cursor: pointer;
 `;
